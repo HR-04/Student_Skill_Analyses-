c63a6e7849eef9f487cf572bf2840c1bb5975f5f
HR_04 <harinirengarajan203@gmail.com>
2024-01-11 21:44:18 +0530

    load model

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ import sys
 import numpy as np 
 import pandas as pd
 import dill
+import pickle
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 
@@ -17,7 +18,7 @@ def save_object(file_path, obj):
         os.makedirs(dir_path, exist_ok=True)
 
         with open(file_path, "wb") as file_obj:
-            dill.dump(obj, file_obj)
+            pickle.dump(obj, file_obj)
 
     except Exception as e:
         raise CustomerException(e, sys)
@@ -52,4 +53,11 @@ def evaluate_models(x_train, y_train,x_test,y_test,models,param):
 
     except Exception as e:
         raise CustomerException(e, sys)
-    
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomerException(e, sys)
